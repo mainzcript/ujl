242d68b122d10229766977b69705434caceecdc2
Denkhaus, Nadine <nadine.denkhaus@students.hs-mainz.de>
2025-10-15 15:22:08 +0200

    feat: 37-medien-subsystem

---
+++ services/media-service/src/collections/Users.ts
@@ -0,0 +1,15 @@
+import type { CollectionConfig } from 'payload'
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  auth: {
+    useAPIKey: true, 
+  },
+  admin: {
+    useAsTitle: 'email',
+  },
+  fields: [
+    // Email added by default
+    // Add more fields as needed
+  ],
+}
