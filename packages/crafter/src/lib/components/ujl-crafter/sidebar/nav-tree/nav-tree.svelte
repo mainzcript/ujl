021fcea57c484672ca926a58b1ef8b4ce0a91800
Nadine Denkhaus <nadine.denkhaus@students.hs-mainz.de>
2026-01-26 14:21:59 +0100

    fix: remove unnessesary padding in navtree

--- packages/crafter/src/lib/components/ujl-crafter/sidebar/nav-tree/nav-tree.svelte
+++ packages/crafter/src/lib/components/ujl-crafter/sidebar/nav-tree/nav-tree.svelte
@@ -89,7 +89,7 @@
 	}
 </script>
 
-<div class="relative flex w-full min-w-0 flex-col p-2" data-crafter="nav-tree">
+<div class="relative flex w-full min-w-0 flex-col" data-crafter="nav-tree">
 	<div
 		class="flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 ring-sidebar-ring outline-hidden transition-[margin,opacity] duration-200 ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0"
 	>
